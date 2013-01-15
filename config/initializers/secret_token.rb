e5ae9309cdb0fb599a5a1b76044be7be4f9c9e3d
Derek <derek_rockwell@uhc.com>
2013-01-15 17:24:22 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BenevitasMvp::Application.config.secret_token = 'c6f7e55410b67d985b4afca5db7685104946f1a88cd659d53f73a95c88a65e21cfe06d8eb748c1ea0d27c96da502c30a1a33711135e196e94d02b30e2f7531c7'
