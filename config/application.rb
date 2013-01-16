79e8bc80901a80a6fba80ad05047aa90cff22cdd
Derek <derek_rockwell@uhc.com>
2013-01-16 16:17:43 -0600

    cleaning up the request form

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,10 @@ module BenevitasMvp
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.paths << "#{Rails.root}/app/assets/font"
 
+    # Precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
